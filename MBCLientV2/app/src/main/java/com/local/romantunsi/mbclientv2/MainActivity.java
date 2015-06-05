f91e2e8b0b65a67b86402ee4b42c641626d08d06
Roma <rama.moldova@gmail.com>
2015-06-05 11:20:34 +0300

    Init

---
+++ MBCLientV2/app/src/main/java/com/local/romantunsi/mbclientv2/MainActivity.java
@@ -0,0 +1,45 @@
+package com.local.romantunsi.mbclientv2;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    TextView tvLogin;
+    TextView tvPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvLogin = (TextView)findViewById(R.id.tvLogin);
+        tvPassword = (TextView)findViewById(R.id.tvPassword);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
